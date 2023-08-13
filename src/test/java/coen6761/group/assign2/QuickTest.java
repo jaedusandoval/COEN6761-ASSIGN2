4ea1ff7e5b3329caffc43160c9caacccebacf4f3
BBeerBear <xiongx659@gmail.com>
2023-08-12 22:42:08 -0400

    change test cases

--- src/test/java/coen6761/group/assign2/QuickTest.java
+++ src/test/java/coen6761/group/assign2/QuickTest.java
@@ -9,17 +9,18 @@ import coen6761.group.assign2.Quick;
 public class QuickTest {
 
 	@Test
-	public void test_i_dcu() {
+	public void test_001() {
 		assertEquals(2, Quick.partition(new Comparable[] {5,3,6,4}, 0, 3));
 	}
 	
 	@Test
-	public void test_j_dcu() {
+	public void test_002() {
 		assertEquals(1, Quick.partition(new Comparable[] {6,8,4,9}, 0, 3));
 	}
 	
 	@Test
-	public void test_i_dpu() {
+	public void test_005() {
 		assertEquals(1, Quick.partition(new Comparable[] {5,4}, 0, 1));
 	}
+	
 }
