18cb555aab4cf87c70dddd87786d1ee68791610e
BBeerBear <xiongx659@gmail.com>
2023-08-12 23:06:21 -0400

    change back test function name

--- src/test/java/coen6761/group/assign2/QuickTest.java
+++ src/test/java/coen6761/group/assign2/QuickTest.java
@@ -9,17 +9,17 @@ import coen6761.group.assign2.Quick;
 public class QuickTest {
 
 	@Test
-	public void test_001() {
+	public void test_i_dcu() {
 		assertEquals(2, Quick.partition(new Comparable[] {5,3,6,4}, 0, 3));
 	}
 	
 	@Test
-	public void test_002() {
+	public void test_j_dcu() {
 		assertEquals(1, Quick.partition(new Comparable[] {6,8,4,9}, 0, 3));
 	}
 	
 	@Test
-	public void test_005() {
+	public void test_i_dpu() {
 		assertEquals(1, Quick.partition(new Comparable[] {5,4}, 0, 1));
 	}
 	
